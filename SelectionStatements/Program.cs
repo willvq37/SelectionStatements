4b82488e8364ad9cedef59b78da04b946fb46107
William Cho <williamcho89@gmail.com>
2021-01-29 21:26:40 -0500

    first save

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is an interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before.  {subject} sounds fun!");
+                    break;
+
+
+            }
+
+        }
+    }
+}
